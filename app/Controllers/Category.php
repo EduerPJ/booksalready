d6c740a10c740766a991ea9e9577164632189371
Eduer Pallares <eduer.pallares@zalvadora.com>
2023-06-03 00:57:56 -0500

    ADD: Add gender endpoints

--- app/Controllers/Category.php
+++ app/Controllers/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
+use App\Models\CategoryModel;
 use CodeIgniter\RESTful\ResourceController;
 
 class Category extends ResourceController
@@ -11,9 +12,10 @@ class Category extends ResourceController
      *
      * @return mixed
      */
-    public function index()
-    {
-        //
+    public function index() {
+        $model = new CategoryModel();
+        $data  = $model->orderBy('id', 'DESC')->findAll();
+        return $this->respond($data);
     }
 
     /**
@@ -21,19 +23,13 @@ class Category extends ResourceController
      *
      * @return mixed
      */
-    public function show($id = null)
-    {
-        //
-    }
-
-    /**
-     * Return a new resource object, with default properties
-     *
-     * @return mixed
-     */
-    public function new()
-    {
-        //
+    public function show($id = null) {
+        $model = new CategoryModel();
+        $data  = $model->where('id', $id)->first();
+        if (empty($data)) {
+            return $this->failNotFound('No category found');
+        }
+        return $this->respond($data);
     }
 
     /**
@@ -41,19 +37,31 @@ class Category extends ResourceController
      *
      * @return mixed
      */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Return the editable properties of a resource object
-     *
-     * @return mixed
-     */
-    public function edit($id = null)
-    {
-        //
+    public function create() {
+        $model = new CategoryModel();
+        $data  = [
+            'name'   => $this->request->getVar('name'),
+        ];
+        $id    = $model->insert($data, true);
+        if (!$id) {
+            return (
+                $this
+                    ->fail(
+                        $model->errors(),
+                        $this->codes['invalid_data'],
+                        'Bad Request',
+                        'Category could not be created'
+                    )
+            );
+        }
+        $response = [
+            'status'   => $this->codes['created'],
+            'error'    => null,
+            'messages' => [
+                'success' => 'Category created successfully',
+            ],
+        ];
+        return $this->respondCreated($response);
     }
 
     /**
@@ -61,9 +69,26 @@ class Category extends ResourceController
      *
      * @return mixed
      */
-    public function update($id = null)
-    {
-        //
+    public function update($id = null) {
+        if (!$id || !is_numeric($id)) {
+            return $this->fail('No identifier');
+        }
+        $data   = [
+            'name'   => $this->request->getVar('name'),
+        ];
+        $model  = new CategoryModel();
+        $result = $model->update($id, $data);
+        if (!$result) {
+            return $this->fail('Category could not be updated');
+        }
+        $response = [
+            'status'   => $this->codes['updated'],
+            'error'    => null,
+            'messages' => [
+                'success' => 'Category updated successfully',
+            ],
+        ];
+        return $this->respond($response);
     }
 
     /**
@@ -71,8 +96,23 @@ class Category extends ResourceController
      *
      * @return mixed
      */
-    public function delete($id = null)
-    {
-        //
+    public function delete($id = null) {
+        if (!$id || !is_numeric($id)) {
+            return $this->fail('No identifier');
+        }
+        $model = new CategoryModel();
+        $data  = $model->where('id', $id)->delete($id);
+        if (!$data) {
+            return $this->failNotFound('No category deleted');
+        }
+        $model->delete($id);
+        $response = [
+            'status'   => $this->codes['deleted'],
+            'error'    => null,
+            'messages' => [
+                'success' => 'Category successfully deleted',
+            ],
+        ];
+        return $this->respondDeleted($response);
     }
 }
