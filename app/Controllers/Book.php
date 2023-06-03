1e1fe145ddbf20a8a741eb625687ff82b4a14a59
Eduer Pallares <eduer.pallares@zalvadora.com>
2023-06-03 00:35:06 -0500

    ADD: endpoints for books

---
+++ app/Controllers/Book.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\BookModel;
+use CodeIgniter\RESTful\ResourceController;
+
+class Book extends ResourceController
+{
+    /**
+     * Return an array of resource objects, themselves in array format
+     *
+     * @return mixed
+     */
+    public function index() {
+        $model = new BookModel();
+        $data  = $model->orderBy('id', 'DESC')->findAll();
+        return $this->respond($data);
+    }
+
+    /**
+     * Return the properties of a resource object
+     *
+     * @return mixed
+     */
+    public function show($id = null) {
+        $model = new BookModel();
+        $data  = $model->where('id', $id)->first();
+        if (!empty($data)) {
+            return $this->respond($data);
+        }
+        return $this->failNotFound('No book found');
+    }
+
+    /**
+     * Create a new resource object, from "posted" parameters
+     *
+     * @return mixed
+     */
+    public function create() {
+        $model = new BookModel();
+        $data  = [
+            'author_id'   => $this->request->getVar('author_id'),
+            'title'       => $this->request->getVar('title'),
+            'description' => $this->request->getVar('description'),
+            'image'       => $this->request->getVar('image'),
+            'gender_id'   => $this->request->getVar('gender_id'),
+            'category_id' => $this->request->getVar('category_id'),
+            'language'    => $this->request->getVar('language'),
+        ];
+        $id    = $model->insert($data, true);
+        if (!$id) {
+            return (
+                $this
+                    ->fail(
+                        $model->errors(),
+                        $this->codes['invalid_data'],
+                        'Bad Request',
+                        'Book could not be created'
+                    )
+            );
+        }
+        $response = [
+            'status'   => $this->codes['created'],
+            'error'    => null,
+            'messages' => [
+                'success' => 'Book created successfully'
+            ],
+        ];
+        return $this->respondCreated($response);
+    }
+
+    /**
+     * Add or update a model resource, from "posted" properties
+     *
+     * @return mixed
+     */
+    public function update($id = null) {
+        if (!$id || !is_numeric($id)) {
+            return $this->fail('No identifier');
+        }
+        $data   = [
+            'author_id'   => $this->request->getVar('author_id'),
+            'title'       => $this->request->getVar('title'),
+            'description' => $this->request->getVar('description'),
+            'image'       => $this->request->getVar('image'),
+            'gender_id'   => $this->request->getVar('gender_id'),
+            'category_id' => $this->request->getVar('category_id'),
+            'language'    => $this->request->getVar('language'),
+        ];
+        $model  = new BookModel();
+        $result = $model->update($id, $data);
+        if (!$result) {
+            return $this->fail('Book could not be updated');
+        }
+        $response = [
+            'status'   => $this->codes['updated'],
+            'error'    => null,
+            'messages' => [
+                'success' => 'Book updated successfully',
+            ],
+        ];
+        return $this->respond($response);
+    }
+
+    /**
+     * Delete the designated resource object from the model
+     *
+     * @return mixed
+     */
+    public function delete($id = null) {
+        if (!$id || !is_numeric($id)) {
+            return $this->fail('No identifier');
+        }
+        $model = new BookModel();
+        $data  = $model->where('id', $id)->delete($id);
+        if (!$data) {
+            return $this->failNotFound('No book deleted');
+        }
+        $model->delete($id);
+        $response = [
+            'status'   => $this->codes['deleted'],
+            'error'    => null,
+            'messages' => [
+                'success' => 'Book successfully deleted',
+            ],
+        ];
+        return $this->respondDeleted($response);
+    }
+}
---
+++ app/Models/BookModel.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BookModel extends Model
+{
+    protected $DBGroup = 'default';
+    protected $table = 'books';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType = 'json';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    // Permitted fields
+    protected $allowedFields = [
+        'author_id',
+        'title',
+        'description',
+        'image',
+        'gender_id',
+        'category_id',
+        'language',
+    ];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+
+    // Validation
+    protected $validationRules = [
+        'author_id'   => 'required',
+        'title'       => 'required|min_length[3]|max_length[255]',
+        'description' => 'permit_empty|max_length[1000]',
+        'image'       => 'permit_empty|valid_url|max_length[255]',
+        'gender_id'   => 'required|integer',
+        'category_id' => 'required|integer',
+        'language'    => 'required|max_length[20]',
+    ];
+
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+}
